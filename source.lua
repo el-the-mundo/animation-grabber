dd30b67bd8be72fd6fd63f61dce6d997a6b2b025
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2023-04-30 15:36:09 +0100

    a

--- source.lua
+++ source.lua
@@ -9,6 +9,25 @@ local animationTable = {}
 rconsolename("Animation Stealer V1 | Light Destroyer ! !!! !!")
 rconsoleprint("Welcome! Type console: cmds to view the list of commands.\n")
 
+function detectInput()
+    consoleInput = rconsoleinput()
+    if consoleInput == "console: cmds" then
+        rconsoleerr("Prefix: console:")
+        rconsoleerr("console: clearTable - Clears the table which contains already played animations. (This is used to re-print animations.)")
+        rconsoleerr("console: clear - Clears whatever has been printed.")
+        rconsoleerr("console: clearAll - Clears both the table and the console.")
+    elseif consoleInput == "console: clearTable" then
+        table.clear(animationTable)
+        rconsoleprint("Cleared Table!\n")
+    elseif consoleInput == "console: clear" then
+        rconsoleclear()
+    elseif consoleInput == "console: clearAll" then
+        rconsoleclear()
+        table.clear(animationTable)
+        rconsoleprint("All has been cleared.\n")
+        end
+end
+
 function animationSteal()
     for i, v in pairs(Humanoid:GetPlayingAnimationTracks()) do
         if not table.find(animationTable, v.Name) then
@@ -17,23 +36,8 @@ function animationSteal()
         end
         
       task.spawn(function()
-        consoleInput = rconsoleinput()
-        if consoleInput == "console: cmds" then
-            rconsoleerr("Prefix: console:")
-            rconsoleerr("console: clearTable - Clears the table which contains already played animations. (This is used to re-print animations.)")
-            rconsoleerr("console: clear - Clears whatever has been printed.")
-            rconsoleerr("console: clearAll - Clears both the table and the console.")
-        elseif consoleInput == "console: clearTable" then
-            table.clear(animationTable)
-            rconsoleprint("Cleared Table!\n")
-        elseif consoleInput == "console: clear" then
-            rconsoleclear()
-        elseif consoleInput == "console: clearAll" then
-            rconsoleclear()
-            table.clear(animationTable)
-            rconsoleprint("All has been cleared.\n")
-            end
-        end)
+        detectInput
+      end)
     end
 end
 
